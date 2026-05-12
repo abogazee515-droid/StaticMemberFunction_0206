598cecde55505c62742c023f714e4150c2d5c413
abogazee515-droid <abogazee515@gmail.com>
2026-05-12 16:52:32 +0700

    Add default constructor to Vehicle class

--- chainfunctioncall/VehicleConstructor.cpp
+++ chainfunctioncall/VehicleConstructor.cpp
@@ -7,6 +7,13 @@ class Vehicle
 private:
     int vehicleId;
     string vehicleName;
+
+public:
+    Vehicle()
+    {
+        vehicleId = 0;
+        vehicleName = "Unknown";
+    }
 };
 
 int main()
