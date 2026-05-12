4354838bcabffdc2e003fab6b97a58f96935188a
abogazee515-droid <abogazee515@gmail.com>
2026-05-12 16:53:50 +0700

    Add constructor with vehicle ID parameter

--- chainfunctioncall/VehicleConstructor.cpp
+++ chainfunctioncall/VehicleConstructor.cpp
@@ -14,6 +14,12 @@ public:
         vehicleId = 0;
         vehicleName = "Unknown";
     }
+
+    Vehicle(int id)
+    {
+        vehicleId = id;
+        vehicleName = "Unknown";
+    }
 };
 
 int main()
