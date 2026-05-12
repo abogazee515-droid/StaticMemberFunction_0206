3fc6fde75f4e25699dfa9a8c8c6f1b494b469a6a
abogazee515-droid <abogazee515@gmail.com>
2026-05-12 16:42:09 +0700

    Add setJudul and getJudul methods

--- chainfunctioncall/chainFunction.cpp
+++ chainfunctioncall/chainFunction.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 class buku {
     string judul;
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this;
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
 };
 
 int main() {
